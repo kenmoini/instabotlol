b4d7295b1f90ce39675e2e0134f87b9b84228929
Ken Moini <ken@kenmoini.com>
2020-04-12 12:43:12 -0500

    Update to Denovo codebase, with new healthz path

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def start_server(path, port=8000):
 
 # Start the server in a new thread
 port = 8000
-daemon = threading.Thread(name='daemon_server', target=start_server, args=('.', port))
+daemon = threading.Thread(name='daemon_server', target=start_server, args=('./healthz/', port))
 daemon.setDaemon(True) # Set as a daemon so it will be killed once the main thread is dead.
 daemon.start()
 
@@ -23,7 +23,7 @@ daemon.start()
 session = InstaPy(username=os.environ.get('USER'), password=os.environ.get('PASS'), headless_browser=True)
 session.login()
 
-session.set_simulation(enabled=os.environ.get('SIMULATION'))
+session.set_simulation(enabled=False)
 
 ## Set quota limits to keep Instagram from banning you because it thinks you're a bot
 
@@ -32,15 +32,15 @@ peak_likes_hourly=51, peak_likes_daily=385, peak_comments_hourly=12, peak_commen
 peak_unfollows_daily=402, peak_server_calls_hourly=None, peak_server_calls_daily=4200)
 
 ## Like the images matching either of these tags and not some others
-session.like_by_tags(["novels", "books", "authors", "writers" "reading"], amount=5)
+session.like_by_tags(["novels", "books", "authors", "writers", "reading", "WriterCommunity", "WritingTips", "WritingPrompts"], amount=8)
 session.set_dont_like(["mein kampf", "nsfw"])
 
 ## Auto-follow half of the time
-session.set_do_follow(True, percentage=50)
+session.set_do_follow(enabled=True, percentage=10, times=1)
 
 ## Auto-Commenting - Disabled by default
-session.set_do_comment(False, percentage=50)
-session.set_comments(["Nice!", "Sweet!", "Beautiful :heart_eyes:"])
+session.set_do_comment(enabled=True, percentage=100)
+session.set_comments(["Nice!", "Sweet!", "I really like this", "Great Content!", "Awesome!", "Interesting!", "We just released a new book, The Stray, care to take a look?", "We're a new indie publisher, check us out!", "How long have you been writing?", "Give us a follow to support indie publishers!", "Do you like prose or poetry most?", "What's your favorite book?"])
 
 ## Additional filters
 session.set_skip_users(skip_private=True, private_percentage=100, skip_no_profile_pic=True, no_profile_pic_percentage=100,
